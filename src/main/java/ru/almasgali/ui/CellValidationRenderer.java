2536817b2cd386d3a8ef6b4ae2b4b3b916cc31d9
Almasgali <ahadyrov@gmail.com>
2024-08-14 14:39:24 +0300

    mask search

--- src/main/java/ru/almasgali/ui/CellValidationRenderer.java
+++ src/main/java/ru/almasgali/ui/CellValidationRenderer.java
@@ -28,13 +28,20 @@ public class CellValidationRenderer extends DefaultTableCellRenderer {
         String textToSearch = searchField.getText();
         if (!text.matches("[0-9a-f]{2}")) {
             cell.setForeground(Color.RED);
-        } else if (text.contains(textToSearch)) {
+        } else if (textToSearch.matches("[0-9a-f]*") && text.contains(textToSearch)) {
             String html = "<html>"
                     + text.substring(0, text.indexOf(textToSearch))
                     + "<span bgcolor=\"yellow\">"
                     + textToSearch
                     + "</span>"
                     + text.substring(text.indexOf(textToSearch) + textToSearch.length())
+                    + "</html>";
+            cell.setText(html);
+        } else if (textToSearch.matches("[0-9a-f.]*") && text.matches(textToSearch)) {
+            String html = "<html>"
+                    + "<span bgcolor=\"yellow\">"
+                    + text
+                    + "</span>"
                     + "</html>";
             cell.setText(html);
         } else {
